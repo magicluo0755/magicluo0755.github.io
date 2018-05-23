f1d3899054088a44351e808d84e32a53a8a956cc
luoxianqiao <luo.xianq@gmail.com>
2018-05-23 16:32:22 +0800

    rebuidling site 2018/05/23 16:32:22

--- deploy.bat
+++ deploy.bat
@@ -11,12 +11,10 @@ set t=%time:~0,8%
 
 set  commitmsg=rebuidling site %d% %t%
 
-echo commit changes to https://github.com/magicluo0755/magicluo0755.github.io.git [Done]
-
 git add -A
 git commit -m "%commitmsg%"
 git push origin master
 
-echo Deploy site to github pages. [Done]
+echo commit changes to https://github.com/magicluo0755/magicluo0755.github.io.git [Done]
 
 pause
