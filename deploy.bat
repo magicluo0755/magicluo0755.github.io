85ef5f2a3fb1c481e1bce0d61075c543666afb52
LaurenceLuo <xqluo2004@163.com>
2025-05-14 09:53:06 +0800

    rebuidling site 2025/05/14  9:53:05

---
+++ deploy.bat
@@ -0,0 +1,20 @@
+@REM !/bin/bat
+@REM 
+@REM Copyright (c) 2023 magicluo.cn
+@REM All Rights Reserved.
+
+
+@echo off
+
+set d=%date:~0,10%
+set t=%time:~0,8%
+
+set  commitmsg=rebuidling site %d% %t%
+
+git add -A
+git commit -m "%commitmsg%"
+git push origin master
+
+echo commit changes to https://github.com/magicluo0755/magicluo0755.github.io.git [Done]
+
+pause
